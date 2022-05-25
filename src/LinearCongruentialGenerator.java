43ceb5bdd7117bf793d5ce5c9a2b040edcd6f4de
Karine <s4kanany@uni-trier.de>
2022-05-25 12:34:41 +0200

    Added code for ex 2

--- src/LinearCongruentialGenerator.java
+++ src/LinearCongruentialGenerator.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class LinearCongruentialGenerator {
 
@@ -15,10 +19,41 @@ public class LinearCongruentialGenerator {
         return i;
     }
 
-    public static void main(String[] args) {
-        LinearCongruentialGenerator lcg = new LinearCongruentialGenerator(64, 5, 12, 55);
+    public static void main(String[] args) throws IOException {
+        LinearCongruentialGenerator lcg1 = new LinearCongruentialGenerator(64, 5, 12, 55);
+        LinearCongruentialGenerator lcg2 = new LinearCongruentialGenerator(64, 5, 13, 55);
+
+        int[] arrayIncrement1 = new int[100];
+        int[] arrayIncrement2 = new int[100];
         for (int i = 0; i < 100; i++) {
-            System.out.println(lcg.randomNumber());
+            arrayIncrement1[i] = lcg1.randomNumber();
         }
+
+        for (int i = 0; i < 100; i++) {
+            arrayIncrement2[i] = lcg2.randomNumber();
+        }
+
+        try {
+            PrintWriter printWriter = new PrintWriter(new File("Random_Numbers.csv"));
+
+            printWriter.write("Increment1, Increment2");
+            printWriter.println();
+
+            for(int i = 0; i < 100; i++) {
+                printWriter.write(Integer.toString(arrayIncrement1[i]));
+                printWriter.write(",");
+                printWriter.write(Integer.toString(arrayIncrement2[i]));
+                printWriter.println();
+
+            }
+
+            printWriter.flush();
+            printWriter.close();
+
+        } catch (IOException ioException) {
+
+        }
+
+
     }
 }
